c500f9223b3172c29ada5c35a0e91adbbba6d975
anna.bielskaya <anna.bielskaya@gmail.com>
2026-04-24 14:55:43 +0300

    Added useEffect hook to catch user's location and sort the list of places

--- src/loc.js
+++ src/loc.js
@@ -2,19 +2,20 @@ function toRad(value) {
   return (value * Math.PI) / 180;
 }
 
-function calculateDistance(lat1, lng1, lat2, lng2) {
+export function calculateDistance(lat1, lon1, lat2, lon2) {
   const R = 6371;
   const dLat = toRad(lat2 - lat1);
-  const dLon = toRad(lng2 - lng1);
-  const l1 = toRad(lat1);
-  const l2 = toRad(lat2);
+  const dLon = toRad(lon2 - lon1);
 
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(l1) * Math.cos(l2);
+    Math.cos(toRad(lat1)) *
+      Math.cos(toRad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const d = R * c;
-  return d;
+  return R * c;
 }
 
 export function sortPlacesByDistance(places, lat, lon) {
